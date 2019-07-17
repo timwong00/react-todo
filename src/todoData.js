97fc2714b69ce2188c3f2f210a8fe72bcb36e9a4
timwong00 <wongtime317@gmail.com>
2019-07-17 11:16:02 -0400

    react to do list phase 3 using props

---
+++ src/todoData.js
@@ -0,0 +1,21 @@
+const todoData = [
+  { id: 1, text: "Do dishes", completed: true },
+  { id: 2, text: "Feed the cats", completed: true },
+  {
+    id: 3,
+    text: "Charge car",
+    completed: true
+  },
+  {
+    id: 4,
+    text: "Continue React",
+    completed: false
+  },
+  {
+    id: 5,
+    text: "Create a portfolio",
+    completed: false
+  }
+];
+
+export default todoData;
